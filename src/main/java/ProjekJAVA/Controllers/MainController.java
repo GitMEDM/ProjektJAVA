b613959ad0adb86faa0b0e9862ec568f0bc88c2d
Mateusz Misiak <m.e.misiak@hotmail.com>
2017-05-31 22:59:18 +0200

    Dodanie górnego menu przycisków dla wyboru labolatorium

--- src/main/java/ProjekJAVA/Controllers/MainController.java
+++ src/main/java/ProjekJAVA/Controllers/MainController.java
@@ -1,9 +1,12 @@
 package ProjekJAVA.Controllers;
 
+import javafx.fxml.FXML;
+
 /**
  * Created by MEDM on 2017-05-31.
  */
 public class MainController {
 
-
+    @FXML
+    private MenuButtonController menuButtonController;
 }
---
+++ src/main/java/ProjekJAVA/Controllers/MenuButtonController.java
@@ -0,0 +1,7 @@
+package ProjekJAVA.Controllers;
+
+/**
+ * Created by MEDM on 2017-05-31.
+ */
+public class MenuButtonController {
+}
